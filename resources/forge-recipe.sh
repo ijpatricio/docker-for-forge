581439622e9b84ea347b1153b5dc495476e83485
Joao Patricio <ijpatricio@gmail.com>
2022-08-12 10:39:00 +0100

    wip

--- resources/forge-recipe.sh
+++ resources/forge-recipe.sh
@@ -4,6 +4,7 @@
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# Focal = Ubuntu 20.04
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 apt install -y docker-ce
